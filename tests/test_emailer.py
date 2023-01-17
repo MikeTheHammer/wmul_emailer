2ac704fea6829161ed5e8a013fec3b2a2e680b9a
MikeTheHammer <MikeTheHammer@users.noreply.github.com>
2023-01-17 11:35:22 -0500

    Refactor tests to remove unnecessarily duplicated code.

--- tests/test_emailer.py
+++ tests/test_emailer.py
@@ -56,6 +56,19 @@ def setup_send_email(mocker, request):
         mocker.Mock(side_effect=mock_server_function)
     )
 
+    def mock_mimetext_function(data):
+        mimetext_contents = {"Body": data}
+        return mimetext_contents
+
+    mock_mimetext = mocker.Mock(side_effect=mock_mimetext_function)
+    mocker.patch(
+        "wmul_emailer.MIMEText",
+        mock_mimetext
+    )
+
+    mock_body = "mock_body"
+    mock_subject = "mock_subject"
+
     mock_destination_email_addresses = ["foo@example.com", "bar@example.com"]
     if use_tuple_of_addresses:
         mock_destination_email_addresses = \
@@ -75,7 +88,10 @@ def setup_send_email(mocker, request):
         mock_port=mock_port, 
         mock_username=mock_username,
         mock_password=mock_password, 
-        mock_from_address=mock_from_address
+        mock_from_address=mock_from_address,
+        mock_body=mock_body,
+        mock_subject=mock_subject,
+        mock_mimetext=mock_mimetext
     )
 
 
@@ -97,7 +113,8 @@ def test_send_email_dest_addresses_not_a_list_tuple_or_str(setup_send_email):
 @pytest.fixture(scope="function")
 def setup_send_email_correctly_created(setup_send_email, mocker):
     emailer = wmul_emailer.EmailSender(
-        destination_email_addresses=setup_send_email.mock_destination_email_addresses,
+        destination_email_addresses=\
+            setup_send_email.mock_destination_email_addresses,
         server_host=setup_send_email.mock_host,
         port=setup_send_email.mock_port,
         user_name=setup_send_email.mock_username,
@@ -105,167 +122,225 @@ def setup_send_email_correctly_created(setup_send_email, mocker):
         from_email_address=setup_send_email.mock_from_address
     )
 
-    def mock_mimetext_function(data):
-        mimetext_contents = {"Body": data}
-        return mimetext_contents
-
-    mock_mimetext = mocker.patch(
-        "wmul_emailer.MIMEText",
-        mocker.Mock(side_effect=mock_mimetext_function)
-    )
-
-    mock_body = "mock_body"
-    mock_subject = "mock_subject"
-
     return make_namedtuple(
         "setup_send_email_correctly_created",
         emailer=emailer, 
-        mock_body=mock_body, 
-        mock_subject=mock_subject,
         setup_send_email=setup_send_email
     )
 
 
-def test_send_email_called_correctly_normal_path(mocker, setup_send_email_correctly_created):
+def test_send_email_called_correctly_normal_path(
+        mocker, 
+        setup_send_email_correctly_created
+    ):
     setup_send_email = setup_send_email_correctly_created.setup_send_email
 
     setup_send_email_correctly_created.emailer.send_email(
-        setup_send_email_correctly_created.mock_body,
-        setup_send_email_correctly_created.mock_subject
+        setup_send_email.mock_body,
+        setup_send_email.mock_subject
     )
 
-    setup_send_email.mock_smtp.assert_called_once_with(setup_send_email.mock_host, port=setup_send_email.mock_port)
-    setup_send_email.mock_server.login.assert_called_once_with(user=setup_send_email.mock_username, password=setup_send_email.mock_password)
+    setup_send_email.mock_smtp.assert_called_once_with(
+        setup_send_email.mock_host, 
+        port=setup_send_email.mock_port
+    )
+
+    setup_send_email.mock_server.login.assert_called_once_with(
+        user=setup_send_email.mock_username, 
+        password=setup_send_email.mock_password
+    )
 
     expected_send_message_calls = []
 
     for this_email_address in setup_send_email.mock_destination_email_addresses:
         this_message = {
-            "Body": setup_send_email_correctly_created.mock_body,
-            "Subject": setup_send_email_correctly_created.mock_subject,
+            "Body": setup_send_email.mock_body,
+            "Subject": setup_send_email.mock_subject,
             "From": setup_send_email.mock_from_address,
             "To": this_email_address
         }
         expected_send_message_calls.append(mocker.call(this_message))
 
-    setup_send_email.mock_server.send_message.assert_has_calls(expected_send_message_calls)
-    assert setup_send_email.mock_server.send_message.call_count == len(setup_send_email.mock_destination_email_addresses)
+    setup_send_email.mock_server.send_message.assert_has_calls(
+        expected_send_message_calls
+    )
+    assert setup_send_email.mock_server.send_message.call_count == \
+        len(setup_send_email.mock_destination_email_addresses)
 
 
-def test_send_email_called_correctly_different_from_address(mocker, setup_send_email_correctly_created):
+def test_send_email_called_correctly_different_from_address(
+        mocker, 
+        setup_send_email_correctly_created
+    ):
+
     setup_send_email = setup_send_email_correctly_created.setup_send_email
 
     new_mock_from_address = "new_mock_from_address"
 
     setup_send_email_correctly_created.emailer.send_email(
-        setup_send_email_correctly_created.mock_body,
-        setup_send_email_correctly_created.mock_subject,
+        setup_send_email.mock_body,
+        setup_send_email.mock_subject,
         from_email_address=new_mock_from_address
     )
 
-    setup_send_email.mock_smtp.assert_called_once_with(setup_send_email.mock_host, port=setup_send_email.mock_port)
-    setup_send_email.mock_server.login.assert_called_once_with(user=setup_send_email.mock_username, password=setup_send_email.mock_password)
+    setup_send_email.mock_smtp.assert_called_once_with(
+        setup_send_email.mock_host, 
+        port=setup_send_email.mock_port
+    )
+    setup_send_email.mock_server.login.assert_called_once_with(
+        user=setup_send_email.mock_username, 
+        password=setup_send_email.mock_password
+    )
 
     expected_send_message_calls = []
 
     for this_email_address in setup_send_email.mock_destination_email_addresses:
         this_message = {
-            "Body": setup_send_email_correctly_created.mock_body,
-            "Subject": setup_send_email_correctly_created.mock_subject,
+            "Body": setup_send_email.mock_body,
+            "Subject": setup_send_email.mock_subject,
             "From": new_mock_from_address,
             "To": this_email_address
         }
         expected_send_message_calls.append(mocker.call(this_message))
 
-    setup_send_email.mock_server.send_message.assert_has_calls(expected_send_message_calls)
-    assert setup_send_email.mock_server.send_message.call_count == len(setup_send_email.mock_destination_email_addresses)
+    setup_send_email.mock_server.send_message.assert_has_calls(
+        expected_send_message_calls
+    )
+
+    assert setup_send_email.mock_server.send_message.call_count == \
+        len(setup_send_email.mock_destination_email_addresses)
 
 
-def test_send_email_called_correctly_different_destination_address(mocker, setup_send_email_correctly_created):
+def test_send_email_called_correctly_different_destination_address(
+        mocker, 
+        setup_send_email_correctly_created
+    ):
+
     setup_send_email = setup_send_email_correctly_created.setup_send_email
 
-    new_mock_destination_addresses = ["new_mock_destination_address_1", "new_mock_destination_address_2"]
+    new_mock_destination_addresses = [
+        "new_mock_destination_address_1", 
+        "new_mock_destination_address_2"
+    ]
 
     setup_send_email_correctly_created.emailer.send_email(
-        setup_send_email_correctly_created.mock_body,
-        setup_send_email_correctly_created.mock_subject,
+        setup_send_email.mock_body,
+        setup_send_email.mock_subject,
         destination_email_addresses=new_mock_destination_addresses
     )
 
-    setup_send_email.mock_smtp.assert_called_once_with(setup_send_email.mock_host, port=setup_send_email.mock_port)
-    setup_send_email.mock_server.login.assert_called_once_with(user=setup_send_email.mock_username, password=setup_send_email.mock_password)
+    setup_send_email.mock_smtp.assert_called_once_with(
+        setup_send_email.mock_host, 
+        port=setup_send_email.mock_port
+    )
+
+    setup_send_email.mock_server.login.assert_called_once_with(
+        user=setup_send_email.mock_username, 
+        password=setup_send_email.mock_password
+    )
 
     expected_send_message_calls = []
 
     for this_email_address in new_mock_destination_addresses:
         this_message = {
-            "Body": setup_send_email_correctly_created.mock_body,
-            "Subject": setup_send_email_correctly_created.mock_subject,
+            "Body": setup_send_email.mock_body,
+            "Subject": setup_send_email.mock_subject,
             "From": setup_send_email.mock_from_address,
             "To": this_email_address
         }
         expected_send_message_calls.append(mocker.call(this_message))
 
-    setup_send_email.mock_server.send_message.assert_has_calls(expected_send_message_calls)
-    assert setup_send_email.mock_server.send_message.call_count == len(setup_send_email.mock_destination_email_addresses)
+    setup_send_email.mock_server.send_message.assert_has_calls(
+        expected_send_message_calls
+    )
+
+    assert setup_send_email.mock_server.send_message.call_count == \
+        len(setup_send_email.mock_destination_email_addresses)
 
 
-def test_send_email_called_correctly_different_from_and_destination_address(mocker, setup_send_email_correctly_created):
+def test_send_email_called_correctly_different_from_and_destination_address(
+        mocker, 
+        setup_send_email_correctly_created
+    ):
+
     setup_send_email = setup_send_email_correctly_created.setup_send_email
 
     new_mock_from_address = "new_mock_from_address"
-    new_mock_destination_addresses = ["new_mock_destination_address_1", "new_mock_destination_address_2"]
+    new_mock_destination_addresses = [
+        "new_mock_destination_address_1", 
+        "new_mock_destination_address_2"
+    ]
 
     setup_send_email_correctly_created.emailer.send_email(
-        setup_send_email_correctly_created.mock_body,
-        setup_send_email_correctly_created.mock_subject,
+        setup_send_email.mock_body,
+        setup_send_email.mock_subject,
         from_email_address=new_mock_from_address,
         destination_email_addresses=new_mock_destination_addresses
     )
 
-    setup_send_email.mock_smtp.assert_called_once_with(setup_send_email.mock_host, port=setup_send_email.mock_port)
-    setup_send_email.mock_server.login.assert_called_once_with(user=setup_send_email.mock_username, password=setup_send_email.mock_password)
+    setup_send_email.mock_smtp.assert_called_once_with(
+        setup_send_email.mock_host, 
+        port=setup_send_email.mock_port
+    )
+
+    setup_send_email.mock_server.login.assert_called_once_with(
+        user=setup_send_email.mock_username, 
+        password=setup_send_email.mock_password
+    )
 
     expected_send_message_calls = []
 
     for this_email_address in new_mock_destination_addresses:
         this_message = {
-            "Body": setup_send_email_correctly_created.mock_body,
-            "Subject": setup_send_email_correctly_created.mock_subject,
+            "Body": setup_send_email.mock_body,
+            "Subject": setup_send_email.mock_subject,
             "From": new_mock_from_address,
             "To": this_email_address
         }
         expected_send_message_calls.append(mocker.call(this_message))
 
-    setup_send_email.mock_server.send_message.assert_has_calls(expected_send_message_calls)
-    assert setup_send_email.mock_server.send_message.call_count == len(setup_send_email.mock_destination_email_addresses)
+    setup_send_email.mock_server.send_message.assert_has_calls(
+        expected_send_message_calls
+    )
+    assert setup_send_email.mock_server.send_message.call_count == \
+        len(setup_send_email.mock_destination_email_addresses)
 
 
-def test_send_email_called_correctly_str_destination_address(setup_send_email_correctly_created):
+def test_send_email_called_correctly_str_destination_address(
+        setup_send_email_correctly_created
+    ):
+
     setup_send_email = setup_send_email_correctly_created.setup_send_email
 
     new_mock_from_address = "new_mock_from_address"
     new_mock_destination_address = "new_mock_destination_address_1"
 
     setup_send_email_correctly_created.emailer.send_email(
-        setup_send_email_correctly_created.mock_body,
-        setup_send_email_correctly_created.mock_subject,
+        setup_send_email.mock_body,
+        setup_send_email.mock_subject,
         from_email_address=new_mock_from_address,
         destination_email_addresses=new_mock_destination_address
     )
 
-    setup_send_email.mock_smtp.assert_called_once_with(setup_send_email.mock_host, port=setup_send_email.mock_port)
-    setup_send_email.mock_server.login.assert_called_once_with(user=setup_send_email.mock_username, password=setup_send_email.mock_password)
+    setup_send_email.mock_smtp.assert_called_once_with(
+        setup_send_email.mock_host, 
+        port=setup_send_email.mock_port
+    )
+
+    setup_send_email.mock_server.login.assert_called_once_with(
+        user=setup_send_email.mock_username, 
+        password=setup_send_email.mock_password
+    )
 
     setup_send_email.mock_server.send_message.assert_called_once_with({
-            "Body": setup_send_email_correctly_created.mock_body,
-            "Subject": setup_send_email_correctly_created.mock_subject,
+            "Body": setup_send_email.mock_body,
+            "Subject": setup_send_email.mock_subject,
             "From": new_mock_from_address,
             "To": new_mock_destination_address
         })
 
 
-def test_send_email_no_destination_email_addresses(setup_send_email, mocker):
+def test_send_email_no_destination_email_addresses(setup_send_email):
     emailer = wmul_emailer.EmailSender(
         server_host=setup_send_email.mock_host,
         port=setup_send_email.mock_port,
@@ -274,66 +349,45 @@ def test_send_email_no_destination_email_addresses(setup_send_email, mocker):
         from_email_address=setup_send_email.mock_from_address
     )
 
-    def mock_mimetext_function(data):
-        mimetext_contents = {"Body": data}
-        return mimetext_contents
-
-    mock_mimetext = mocker.patch(
-        "wmul_emailer.MIMEText",
-        mocker.Mock(side_effect=mock_mimetext_function)
-    )
-
-    mock_body = "mock_body"
-    mock_subject = "mock_subject"
-
     with pytest.raises(ValueError) as ve:
         emailer.send_email(
-            email_body=mock_body,
-            email_subject=mock_subject
+            email_body=setup_send_email.mock_body,
+            email_subject=setup_send_email.mock_subject
         )
-        assert "destination_email_addresses must be provided to either the constructor or to the send_email function" in str(ve)
+        assert "destination_email_addresses must be provided to either the " \
+            "constructor or to the send_email function" in str(ve)
 
     setup_send_email.mock_smtp.assert_not_called()
-    mock_mimetext.assert_not_called()
+    setup_send_email.mock_mimetext.assert_not_called()
     setup_send_email.mock_server.login.assert_not_called()
     setup_send_email.mock_server.send_message.assert_not_called()
 
 
-def test_send_email_no_from_email_address(setup_send_email, mocker):
+def test_send_email_no_from_email_address(setup_send_email):
     emailer = wmul_emailer.EmailSender(
-        destination_email_addresses=setup_send_email.mock_destination_email_addresses,
+        destination_email_addresses=\
+            setup_send_email.mock_destination_email_addresses,
         server_host=setup_send_email.mock_host,
         port=setup_send_email.mock_port,
         user_name=setup_send_email.mock_username,
         password=setup_send_email.mock_password
     )
 
-    def mock_mimetext_function(data):
-        mimetext_contents = {"Body": data}
-        return mimetext_contents
-
-    mock_mimetext = mocker.patch(
-        "wmul_emailer.MIMEText",
-        mocker.Mock(side_effect=mock_mimetext_function)
-    )
-
-    mock_body = "mock_body"
-    mock_subject = "mock_subject"
-
     with pytest.raises(ValueError) as ve:
         emailer.send_email(
-            email_body=mock_body,
-            email_subject=mock_subject
+            email_body=setup_send_email.mock_body,
+            email_subject=setup_send_email.mock_subject
         )
-        assert "from_email_address must be provided to either the constructor or to the send_email function." in str(ve)
+        assert "from_email_address must be provided to either the constructor "\
+            "or to the send_email function." in str(ve)
 
     setup_send_email.mock_smtp.assert_not_called()
-    mock_mimetext.assert_not_called()
+    setup_send_email.mock_mimetext.assert_not_called()
     setup_send_email.mock_server.login.assert_not_called()
     setup_send_email.mock_server.send_message.assert_not_called()
 
 
-def test_send_email_no_from_and_no_destination_email_address(setup_send_email, mocker):
+def test_send_email_no_from_and_no_destination_email_address(setup_send_email):
     emailer = wmul_emailer.EmailSender(
         server_host=setup_send_email.mock_host,
         port=setup_send_email.mock_port,
@@ -341,26 +395,15 @@ def test_send_email_no_from_and_no_destination_email_address(setup_send_email, m
         password=setup_send_email.mock_password
     )
 
-    def mock_mimetext_function(data):
-        mimetext_contents = {"Body": data}
-        return mimetext_contents
-
-    mock_mimetext = mocker.patch(
-        "wmul_emailer.MIMEText",
-        mocker.Mock(side_effect=mock_mimetext_function)
-    )
-
-    mock_body = "mock_body"
-    mock_subject = "mock_subject"
-
     with pytest.raises(ValueError) as ve:
         emailer.send_email(
-            email_body=mock_body,
-            email_subject=mock_subject
+            email_body=setup_send_email.mock_body,
+            email_subject=setup_send_email.mock_subject
         )
-        assert "must be provided to either the constructor or to the send_email function." in str(ve)
+        assert "must be provided to either the constructor or to the "\
+            "send_email function." in str(ve)
 
     setup_send_email.mock_smtp.assert_not_called()
-    mock_mimetext.assert_not_called()
+    setup_send_email.mock_mimetext.assert_not_called()
     setup_send_email.mock_server.login.assert_not_called()
     setup_send_email.mock_server.send_message.assert_not_called()
